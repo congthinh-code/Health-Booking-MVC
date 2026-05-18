f4175a405ff9ba08c46a10ece3d1aabe792b314b
congthinh-code <congthinh1712@gmail.com>
2026-05-18 15:50:04 +0700

    new

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Health_Booking_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
