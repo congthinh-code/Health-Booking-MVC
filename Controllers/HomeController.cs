3ccb319d883f6390126953a6d6d97d5671e7b075
CamThien <ngthcamthien27td03@gmail.com>
2026-06-03 17:49:36 +0700

    giaodiendatlichkham

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,34 @@
 using Health_Booking_MVC.Models;
+using Health_Booking_MVC.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Health_Booking_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly HealthBookingDbContext _context;
+
+        public HomeController(HealthBookingDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new BookingViewModel
+            {
+                Hospitals = await _context.Hospitals
+                    .OrderBy(h => h.Name)
+                    .ToListAsync(),
+
+                Specializations = await _context.Specializations
+                    .OrderBy(s => s.Name)
+                    .ToListAsync()
+            };
+
+            return View(model);
         }
 
         public IActionResult Privacy()
@@ -19,7 +39,10 @@ namespace Health_Booking_MVC.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
---
+++ Models/ViewModels/BookingViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Health_Booking_MVC.Models.ViewModels
+{
+    public class BookingViewModel
+    {
+        public List<Hospital> Hospitals { get; set; } = new();
+        public List<Specialization> Specializations { get; set; } = new();
+    }
+}
