cb31a3ecc63e13fdfdbd19c3a610209a5f572c57
I'm BT <habaotran1003205@gmail.com>
2026-06-04 20:43:24 +0700

    themdb

--- Controllers/CSYTController.cs
+++ Controllers/CSYTController.cs
@@ -1,22 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Health_Booking_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Health_Booking_MVC.Controllers
 {
     public class CSYTController : Controller
     {
-        public IActionResult Index()
+        private readonly HealthBookingDbContext _context;
+
+        public CSYTController(HealthBookingDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> BVCong()
+        {
+            var dsBenhVien = await _context.Hospitals
+                .Where(h => h.Description == "Bệnh viện công")
+                .ToListAsync();
+
+            return View(dsBenhVien);
+        }
+        public async Task<IActionResult> BVTu()
+        {
+            var dsBenhVien = await _context.Hospitals
+                .Where(h => h.Description == "Bệnh viện tư")
+                .ToListAsync();
+
+            return View(dsBenhVien);
         }
 
-        public IActionResult BVCong()
-        {
-            return View();
-        }
-
-        public IActionResult BVTu()
-        {
-            return View();
-        }
     }
 }
