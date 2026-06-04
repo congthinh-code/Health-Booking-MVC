524adf5c05aef30e85812ba6e340ff95bc177bb5
pthao-nb <p.thao.221205@gmail.com>
2026-06-04 17:05:13 +0700

    trangcanhan

---
+++ Models/ViewModels/PatientProfileViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Health_Booking_MVC.Models.ViewModels
+{
+    public class PatientProfileViewModel
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string? Avatar { get; set; }
+
+        public string? CurrentPassword { get; set; }
+
+        public string? NewPassword { get; set; }
+
+        public string? ConfirmPassword { get; set; }
+    }
+}
